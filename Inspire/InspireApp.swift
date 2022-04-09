f17b24462d2ab35815cc0def2edcba56730c7aa4
Mahmudul Hasan <mhasan341@gmail.com>
2022-04-10 02:34:43 +0600

    Initial Commit

---
+++ Inspire/InspireApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspireApp.swift
+//  Inspire
+//
+//  Created by Mahmudul Hasan on 2022-04-10.
+//
+
+import SwiftUI
+
+@main
+struct InspireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
